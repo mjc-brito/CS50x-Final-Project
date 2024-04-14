60d009dcf16ddd05ea1ae51575fdf1681d659b57
Matheus <mathcaceres@hotmail.com>
2024-04-14 22:38:20 +0100

    Add copy button

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 This repository contains my final project for the CS50x Introduction to Computer Science course offered by Harvard University. The project is a web application built using Flask framework and incorporates various software development and web programming languages covered in the course, including Python, JavaScript, HTML, and CSS. The application is designed to serve as an random strong password generator.
 ## How To Use It
 Enter a number between 8 and 16 in the ‘Password Length’ field and click on ‘Generate’. Your new strong and random password will appear below.
-## Future Work
-- Add a "copy" button (to copy the password with just one click)
--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
     let canvas = document.querySelector("canvas");
     
-        let ctx = canvas.getContext("2d");
-        let width = canvas.width = window.innerWidth;
-        let height = canvas.height = window.innerHeight;
-        let str = "A+jk js:2 @dfs17 tr YY ufds 28Tz op0 Ig_ 34*= {23RT &qwK #fKb $hJ ()! :? &*dUF WE4 5$K bUi";
-        let matrix = str.split("");
-        let font = 12;
-        let col = width / font;
-        let arr = [];
-        
-        for (let i = 0; i < col; i++)
+    let ctx = canvas.getContext("2d");
+    let width = canvas.width = window.innerWidth;
+    let height = canvas.height = window.innerHeight;
+    let str = "A+jk js:2 @dfs17 tr YY ufds 28Tz op0 Ig_ 34*= {23RT &qwK #fKb $hJ ()! :? &*dUF WE4 5$K bUi";
+    let matrix = str.split("");
+    let font = 12;
+    let col = width / font;
+    let arr = [];
+    
+    for (let i = 0; i < col; i++)
+    {
+        arr[i] = 1;
+    }
+    
+    const draw = () => {
+        ctx.fillStyle = "rgba(0,0,0,0.05)";
+        ctx.fillRect(0,0,width,height);
+        ctx.fillStyle = "#009000";
+        ctx.font = `${font}px system-ui`;
+    
+        for (let i = 0; i < arr.length; i++)
         {
-            arr[i] = 1;
-        }
+            let txt = matrix[Math.floor(Math.random() * matrix.length)];
+            ctx.fillText(txt, i * font, arr[i] * font);
         
-        const draw = () => {
-            ctx.fillStyle = "rgba(0,0,0,0.05)";
-            ctx.fillRect(0,0,width,height);
-            ctx.fillStyle = "#009000";
-            ctx.font = `${font}px system-ui`;
-        
-            for (let i = 0; i < arr.length; i++)
+            if (arr[i] * font > height && Math.random() > 0.975)
             {
-                let txt = matrix[Math.floor(Math.random() * matrix.length)];
-                ctx.fillText(txt, i * font, arr[i] * font);
-            
-                if (arr[i] * font > height && Math.random() > 0.975)
-                {
-                    arr[i] = 0;
-                }
-                arr[i]++;
-            
+                arr[i] = 0;
             }
+            arr[i]++;
+        
         }
-        
-        setInterval(draw, 25);
-        
-        window.addEventListener("resize", () => location.reload());
+    }
+    
+    setInterval(draw, 25);
+    
+    window.addEventListener("resize", () => location.reload());
+    
+    
+    // Get the button element
+    var copyButton = document.getElementById("copyButton");
+    // Add click event listener to the button
+    copyButton.addEventListener("click", function() {
+        // Get the content of the <p> tag
+        var passwordContent = document.querySelector(".result").textContent;
+        // Copy the content to the clipboard
+        navigator.clipboard.writeText(passwordContent)
+            .then(function() {
+                // Alert user that content was copied successfully
+                alert("Content copied to clipboard: " + passwordContent);
+            })
+            .catch(function(error) {
+                // Handle error
+                console.error("Could not copy content: ", error);
+            });
+    });
+
 });
